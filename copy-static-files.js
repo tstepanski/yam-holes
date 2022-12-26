6201aca44c82ca82ee343e036b01a896ba69d638
Timothy M. Stepanski <timothy@timothystepanski.com>
2022-12-25 23:42:02 -0500

    made pipeline fixes

--- copy-static-files.js
+++ copy-static-files.js
@@ -1,11 +1,13 @@
 const fileSystem = require("fs");
 
-fileSystem.copyFileSync("src/styles.css", "dist/styles.css");
+fileSystem.readdirSync("./src").forEach(console.log);
+
+fileSystem.copyFileSync("./src/styles.css", "./dist/styles.css");
 
 const markup = fileSystem
-	.readFileSync("src/index.html")
+	.readFileSync("./src/index.html")
 	.toString()
 	.replace("main.ts", "main.min.js")
 	.replace("styles.css", "styles.min.css");
 
-fileSystem.writeFileSync("dist/index.html", markup);
+fileSystem.writeFileSync("./dist/index.html", markup);
